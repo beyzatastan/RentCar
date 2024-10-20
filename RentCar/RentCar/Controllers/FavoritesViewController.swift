9d0fb6ac7a9401c7c3774dddaaf783466b0c3aa3
Beyza Taştan <bbeyzatastan@gmail.com>
2024-10-20 15:10:11 +0300

    update 1.5

--- RentCar/RentCar/Controllers/FavoritesViewController.swift
+++ RentCar/RentCar/Controllers/FavoritesViewController.swift
@@ -7,16 +7,20 @@
 
 import UIKit
 
-class FavoritesViewController: UIViewController {
+class FavoritesViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var favsView: UIView!
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    let names = ["honda civic","supra"]
+    let photos = ["lale","kule"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
         favsView.layer.cornerRadius = 10
-        
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
     @IBAction func homeButton(_ sender: Any) {
@@ -34,5 +38,23 @@ class FavoritesViewController: UIViewController {
         self.navigationController?.pushViewController(viewcontroller, animated: false)
     }
     
+    
+}
+extension FavoritesViewController{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "favs", for: indexPath) as! FavoritesCollectionViewCell
+        let imageName = photos[indexPath.row]
+        cell.imageView?.image = UIImage(named: imageName)
+        cell.nameText.text = names[indexPath.row]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: 160, height: 240)
+        }
     
 }
--- RentCar/RentCar/ViewCells/FavoritesCollectionViewCell.swift
+++ RentCar/RentCar/ViewCells/FavoritesCollectionViewCell.swift
@@ -9,4 +9,7 @@ import UIKit
 
 class FavoritesCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var nameText: UILabel!
+    @IBOutlet weak var rateText: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
 }
